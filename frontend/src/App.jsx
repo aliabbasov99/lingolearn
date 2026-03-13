322a6c36153e69e18fc51def3ffa84c820fe8ab5
Ali Abbasov <aliabbasov9282@gmail.com>
2026-03-13 12:39:25 +0400

    Add MyDecks route to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
+import MyDecks from "./pages/MyDecks";
 import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
 import DeckSession from "./pages/DeckSession";
@@ -17,6 +18,7 @@ export default function App() {
       <Route path="/register" element={<Register />} />
       <Route element={<Layout />}>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/decks" element={<MyDecks />} />
         <Route path="/decks/:deckId" element={<DeckSession />} />
         <Route path="/decks/study/:deckId" element={<StudySession />} />
         <Route path="/profile" element={<Profile />} />
